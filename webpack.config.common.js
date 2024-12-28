dc933470634c98729e25313e3544ab44ce08440d
Dmitry Merzlyakov <jagotti@yandex.ru>
2024-12-29 00:28:14 +0300

    Revert "fix: fix for deploy"
    This reverts commit 5c3c9888ae157cdcb4d50c1510d1f9c8604368b7.

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,48 +1,46 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  context: path.resolve(__dirname, "src"),
-  entry: "./index.ts",
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.ts',
   output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./index.html"),
-    }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, "public/assets"),
-          to: path.resolve(__dirname, "dist"),
-        },
-      ],
-    }),
-    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin(
+      {template: path.resolve(__dirname, 'public/index.html')}
+    ),
+    new CopyPlugin(
+      {patterns: [{
+        from: path.resolve(__dirname, 'public/assets'),
+        to: path.resolve(__dirname, 'dist')
+      }]}
+    ),
+    new MiniCssExtractPlugin()
   ],
   module: {
     rules: [
       {
         test: /\.[tj]s$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.(scss|css)$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-    ],
+        type: 'asset/resource',
+      }
+    ]
   },
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.js'],
   },
-};
+}
