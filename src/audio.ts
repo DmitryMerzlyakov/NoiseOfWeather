2667bdbe2943ed1d4f42dfcb323f15f9f6e5bc89
Dmitry Merzlyakov <jagotti@yandex.ru>
2024-12-29 00:07:42 +0300

    fix: all fix

--- src/audio.ts
+++ src/audio.ts
@@ -1,74 +1,66 @@
-// import { TApllication } from "..";
-// import { button, image } from "./weatherItem";
+import { TApllication } from "..";
 
-let currentAudio: HTMLAudioElement | null = null
-let currentVolume: number = 1
-let currentCardIndex: number = -1
+let currentAudio: HTMLAudioElement | null = null;
+let currentVolume: number = 1;
+let currentCardIndex: number = -1;
 
-// export const toggleAudio = (index: number, cards: TApllication[]) => {
-//   const isCurrentCard = currentCardIndex === index
-//   currentAudio && 
-//     (isCurrentCard ? 
-//       (currentAudio.paused ? 
-//         (currentAudio.play(), updateIcon(currentAudio.paused))
-//         :
-//         (currentAudio.pause(), updateIcon(currentAudio.paused))
-//       )
-//       :
-//       (() => {currentAudio.pause(), currentAudio.currentTime = 0})()
-//     )
-//   !isCurrentCard && (() => {
-//     currentAudio = new Audio(cards[index].sound)
-//     currentAudio.volume = currentVolume
-//     currentAudio.play()
-//     currentCardIndex = index
-//   })()
-// }
+export const toggleAudio = (
+  index: number,
+  cards: TApllication[],
+  button: HTMLButtonElement,
+) => {
+  const isCurrentCard = currentCardIndex === index;
+  currentAudio &&
+    (isCurrentCard
+      ? currentAudio.paused
+        ? (currentAudio.play(),
+          updateIcon(currentAudio.paused, button, cards[index]))
+        : (currentAudio.pause(),
+          updateIcon(currentAudio.paused, button, cards[index]))
+      : (() => {
+          currentAudio.pause(), (currentAudio.currentTime = 0);
+        })());
+  !isCurrentCard &&
+    (() => {
+      currentAudio = new Audio(cards[index].sound);
+      currentAudio.volume = currentVolume;
+      currentAudio.play();
+      currentCardIndex = index;
+      resetOtherIcons(cards, index);
+    })();
+};
 
-// const updateIcon = (isUpdate: boolean) => {
-//   const imagePause = document.createElement('img')
-//   imagePause.classList.add('item__imgPause')
-//   imagePause.src = '../assets/icons/pause.svg'
-//   imagePause.alt = 'pause'
-//   isUpdate ? 
-//     (button?.appendChild(imagePause), button.querySelector('.item__img')?.remove())
-//     : 
-//     (image && button?.appendChild(image), button.querySelector('.item__imgPause')?.remove())
-// }
-
-const AudioContext = new (window.AudioContext || window.AudioContext)();
-let sourceNode: MediaElementAudioSourceNode | null = null;
-let isPlaying = false;
-
-export async function toggleAudio(fileUrl: string) {
-  if (!isPlaying) {
-    const audioElement = new Audio(fileUrl);
-    sourceNode = AudioContext.createMediaElementSource(audioElement);
-    sourceNode.connect(AudioContext.destination);
-    
-    try {
-      await audioElement.play();
-      isPlaying = true;
-    } catch (error) {
-      console.error("Ошибка воспроизведения:", error);
-    }
-  } else {
-    if (sourceNode) {
-      sourceNode.mediaElement.pause();
-      isPlaying = false;
-    }
-  }
-}
-
-// document.getElementById('playButton')?.addEventListener('click', () => {
-//   playOrPause('path/to/your/audio.mp3'); // Укажите путь к вашему MP3 файлу
-// });
-
+const setVolume = (audio: HTMLAudioElement, volume: number) => {
+  audio.volume = volume;
+};
 
 export const audioControl = (element: HTMLInputElement) => {
-  // element.addEventListener('input', (event: Event) => {
-  //   const target = event.target as HTMLInputElement
-  //   currentVolume = parseFloat(target.value)
-  //   currentAudio && (currentAudio.volume = currentVolume)
-  // })
-}
+  element.addEventListener("input", () => {
+    currentAudio && setVolume(currentAudio, parseFloat(element.value));
+  });
+};
+
+const updateIcon = (
+  isUpdate: boolean,
+  button: HTMLButtonElement,
+  card: TApllication,
+) => {
+  const imagePause = document.createElement("img");
+  imagePause.classList.add("item__imgPause");
+  imagePause.src = "../assets/icons/pause.svg";
+  imagePause.alt = "pause";
+  isUpdate
+    ? button.querySelector("img")?.setAttribute("src", imagePause.src)
+    : button.querySelector("img")?.setAttribute("src", card.icon);
+};
+
+const resetOtherIcons = (cards: TApllication[], index: number) => {
+  cards.forEach((otherCard) => {
+    if (otherCard.id !== cards[index].id) {
+      const otherButton = document.querySelector(
+        `button:nth-of-type(${otherCard.id + 1})`,
+      );
+      otherButton?.querySelector("img")?.setAttribute("src", otherCard.icon);
+    }
+  });
+};
--- src/weatherItem.ts
+++ src/weatherItem.ts
@@ -1,84 +1,43 @@
-import { audioControl } from './audio'
+import { audioControl, toggleAudio } from "./audio";
 
-import { TApllication } from '..'
+import { TApllication } from "..";
 
-let currentAudio: HTMLAudioElement | null = null;
-let currentVolume: number = 1
-let currentCardIndex: number = -1
-const buttonsWrapper = document.createElement('div')
-buttonsWrapper.classList.add('wrapper')
-const volumeControl = document.createElement('input')
-volumeControl.type = 'range'
-volumeControl.min = '0'
-volumeControl.max = '1'
-volumeControl.step = '0.1'
-volumeControl.value = '0.5'
+const buttonsWrapper = document.createElement("div");
+buttonsWrapper.classList.add("wrapper");
+const volumeControl = document.createElement("input");
+volumeControl.type = "range";
+volumeControl.min = "0";
+volumeControl.max = "1";
+volumeControl.step = "0.1";
+volumeControl.value = "0.5";
 
-export const createWeatherItem = (items: TApllication[], paretn: HTMLDivElement) => {
+const imagePause = document.createElement("img");
+imagePause.classList.add("item__imgPause");
+imagePause.src = "../assets/icons/pause.svg";
+imagePause.alt = "pause";
+
+export const createWeatherItem = (
+  items: TApllication[],
+  paretn: HTMLDivElement,
+) => {
   items.forEach((card, index) => {
-    const button = document.createElement('button')
-    button.classList.add('item')
-    button.style.backgroundImage = `url(${card.img})`
-    button.style.backgroundSize = 'cover'
-    button.style.backgroundPosition = 'center'
-    button.innerHTML = `<img src="${card.icon}" alt="${card.weatherType} icon">`
-    buttonsWrapper.appendChild(button)
-    
-    button.addEventListener('click', () => {
-      // if (currentAudio) {
-      //   currentAudio.pause()
-      //   currentAudio.currentTime = 0
-      // }
+    const button = document.createElement("button");
+    button.classList.add("item");
+    button.style.backgroundImage = `url(${card.img})`;
+    button.style.backgroundSize = "cover";
+    button.style.backgroundPosition = "center";
+    button.innerHTML = `<img src="${card.icon}" alt="${card.weatherType} icon">`;
+    buttonsWrapper.appendChild(button);
 
-      // if (!currentAudio || currentAudio.src !== card.sound) {
-      //   currentAudio = new Audio(card.sound)
-      //   currentAudio.volume = parseFloat(volumeControl.value)
-      //   currentAudio.play()
-      // } else {
-      //   if (currentAudio.paused) {
-      //   currentAudio.volume = parseFloat(volumeControl.value)
-      //   currentAudio.play()
-      //   } else {
-      //   currentAudio.pause()
-      //   }
-      // }
-      toggleAudio(index, items)
-    })
-  })
-  paretn.appendChild(buttonsWrapper)
-  paretn.appendChild(volumeControl)
-}
+    button.addEventListener("click", () => {
+      paretn.style.backgroundImage = `url(${card.img})`;
+      paretn.style.backgroundSize = "cover";
+      paretn.style.backgroundPosition = "center";
 
-const setVolume = (audio: HTMLAudioElement, volume: number) => {
-  audio.volume = volume
+      toggleAudio(index, items, button);
+    });
+  });
+  audioControl(volumeControl);
+  paretn.appendChild(buttonsWrapper);
+  paretn.appendChild(volumeControl);
 };
-
-volumeControl.addEventListener('input', () => {
-  if (currentAudio) {
-    setVolume(currentAudio, parseFloat(volumeControl.value))
-  }
-})
-
-const toggleAudio = (index: number, cards: TApllication[]) => {
-    const isCurrentCard = currentCardIndex === index
-    currentAudio && 
-      (isCurrentCard ? 
-        (currentAudio.paused ? 
-          (currentAudio.play()
-          // updateIcon(currentAudio.paused)
-        )
-          :
-          (currentAudio.pause()
-          // updateIcon(currentAudio.paused)
-        )
-        )
-        :
-        (() => {currentAudio.pause(), currentAudio.currentTime = 0})()
-      )
-    !isCurrentCard && (() => {
-      currentAudio = new Audio(cards[index].sound)
-      currentAudio.volume = currentVolume
-      currentAudio.play()
-      currentCardIndex = index
-    })()
-  }
